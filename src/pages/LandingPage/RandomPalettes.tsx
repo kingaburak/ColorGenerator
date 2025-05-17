6e6f05b6f44d6e33c91ab50b2c592037ed1a6c5e
Kinga Burak <kingaburak1@gmail.com>
2025-05-17 15:06:18 +0200

    framer motion

--- src/pages/LandingPage/RandomPalettes.tsx
+++ src/pages/LandingPage/RandomPalettes.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Palette from "./Palette";
+import { motion, useInView } from "framer-motion";
 
 const getRandomHex = (): string => {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -17,6 +18,9 @@ const fetchPalette = async (): Promise<string[]> => {
 
 const RandomPalettes: React.FC = () => {
   const [palettes, setPalettes] = useState<string[][]>([]);
+
+  const titleRef = useRef(null);
+  const isInView = useInView(titleRef, { once: true });
 
   useEffect(() => {
     const generatePalettes = async () => {
@@ -36,18 +40,26 @@ const RandomPalettes: React.FC = () => {
       id="palettes"
       className="max-w-7xl mx-auto px-6 md:px-20 py-12 bg-[#FFF7F0] text-center"
     >
-      <h2 className="text-3xl font-bold mb-2">
-        <span className="text-darkblue">Zobacz </span>
-        <span className="text-green">przykładowe </span>
-        <span className="text-darkpink">palety </span>
-        <span className="text-blue">kolorów! </span>
-      </h2>
-      <p className="text-black max-w-3xl mx-auto mb-12">
-        Zainspiruj się gotowymi zestawieniami, które pasują do każdego projektu!
-        Niezależnie od tego, czy tworzysz stronę internetową, aranżujesz
-        wnętrze, czy projektujesz logo - znajdziesz tu paletę, która idealnie
-        wpisuje się w Twoje potrzeby.
-      </p>
+      <motion.div
+        ref={titleRef}
+        initial={{ opacity: 0, y: 50 }}
+        animate={isInView ? { opacity: 1, y: 0 } : {}}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+      >
+        <h2 className="text-3xl font-bold mb-2">
+          <span className="text-darkblue">Zobacz </span>
+          <span className="text-green">przykładowe </span>
+          <span className="text-darkpink">palety </span>
+          <span className="text-blue">kolorów! </span>
+        </h2>
+        <p className="text-black max-w-3xl mx-auto mb-12">
+          Zainspiruj się gotowymi zestawieniami, które pasują do każdego
+          projektu! Niezależnie od tego, czy tworzysz stronę internetową,
+          aranżujesz wnętrze, czy projektujesz logo - znajdziesz tu paletę,
+          która idealnie wpisuje się w Twoje potrzeby.
+        </p>
+      </motion.div>
+
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 justify-items-center relative">
         {palettes.map((colors, idx) => (
           <Palette key={idx} colors={colors} index={idx} />
